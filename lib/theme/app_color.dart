3b716e8dee70c96d859ac988e2dc81336971e079
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2024-01-15 10:05:09 +0600

    initial setup is done

---
+++ lib/theme/app_color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const Color primary = Color(0xFFb70000);
+  static const Color secondary = Color(0xFFff5735);
+  static const Color yellow = Color(0xFFFFCC00);
+  static const Color red = Color(0xFFb70000);
+  static const Color green = Color(0xFF4CE5B1);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFf4f4f2);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color grey = Colors.grey;
+  static const Color text = Color(0xFF222222);
+  static const Color icon = Color(0xFF9B9B9B);
+}
